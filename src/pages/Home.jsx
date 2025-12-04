681e7543482c26d6e5f11be5a394ff177280d345
HadleyKing <charles.hadley.king@gmail.com>
2025-12-04 14:47:35 -0500

    tight home Changes to be committed: 	modified:   src/App.css 	modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 //src/pages/Home.jsx
 
-import { Space, Tooltip } from "antd";
+import { Card, Space, Tooltip } from "antd";
 
 export default function Home() {
   return (
-    <>
+    <div className="about">
         <h2>About the LoTempio Lab</h2>
         <p>The LoTempio Lab integrates computational genomics, translational bioinformatics, and bioethics to advance the responsible use of informatic technologies in human health and research. Our work spans three complementary areas: the basic science of human genome informatics, the translational science of informatic technologies, and empirical and conceptual bioethics.</p>
-    </>
+    </div>
   );
 }
