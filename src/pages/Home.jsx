bf9b1edfa50d2017e679f5defd6fd755e7c5df8a
HadleyKing <charles.hadley.king@gmail.com>
2026-01-06 18:07:57 -0500

    Formatting Home page
    Changes to be committed:
    	modified:   .gitignore
    	new file:   dist/assets/23andMeBankruptcy-DvMuQvMw.png
    	deleted:    dist/assets/index-BOvAN_OY.js
    	renamed:    dist/assets/index-D2CdZrhl.css -> dist/assets/index-PX2mvD8C.css
    	new file:   dist/assets/index-uYaG0orX.js
    	modified:   dist/index.html
    	modified:   src/App.css
    	new file:   src/assets/23andMeBankruptcy.png
    	modified:   src/pages/Home.jsx
    	modified:   src/pages/Research.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 //src/pages/Home.jsx
 
 import { Card, Space, Tooltip } from "antd";
+import Bankruptcy from "../assets/23andMeBankruptcy.png";
 
 export default function Home() {
   return (
     <div className="about">
-        <h2>About the LoTempio Lab</h2>
-        <p>The LoTempio Lab integrates computational genomics, translational bioinformatics, and bioethics to advance the responsible use of informatic technologies in human health and research. Our work spans three complementary areas: the basic science of human genome informatics, the translational science of informatic technologies, and empirical and conceptual bioethics.</p>
+      <h2>About the LoTempio Lab</h2>
+      <p>The LoTempio Lab integrates computational genomics, translational bioinformatics, and bioethics to advance the responsible use of informatic technologies in human health and research. Our work spans three complementary areas: the basic science of human genome informatics, the translational science of informatic technologies, and empirical and conceptual bioethics.</p>
+      <Card title="News and Events">
+        <a href="https://www.nature.com/articles/s41588-025-02423-8" target="_blank">
+          <img src={Bankruptcy}/>
+        </a>
+      </Card>
     </div>
   );
 }
